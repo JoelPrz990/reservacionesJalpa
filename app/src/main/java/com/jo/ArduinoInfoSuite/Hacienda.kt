79ac189892d4199e68ae1a87b3fa05905cfee608
lo397 <lo3977884@gmail.com>
2022-12-01 01:46:51 -0600

    Login Listo

---
+++ app/src/main/java/com/jo/ArduinoInfoSuite/Hacienda.kt
@@ -0,0 +1,17 @@
+package com.jo.ArduinoInfoSuite
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.codingstuff.loginandsignup.R
+import com.codingstuff.loginandsignup.databinding.ActivityHaciendaBinding
+
+class Hacienda : AppCompatActivity() {
+    private lateinit var binding: ActivityHaciendaBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+
+        binding
+    }
+}
